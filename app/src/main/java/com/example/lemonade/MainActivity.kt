5e0a3bd0d4f97a2fccc980a2403dcab109b0b75a
cheche-casa <cheche.casa@outlook.es>
2023-05-08 20:30:53 +0200

    Antes de random

--- app/src/main/java/com/example/lemonade/MainActivity.kt
+++ app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.CutCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -26,7 +27,6 @@ import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign.Companion.Center
-import androidx.compose.ui.text.style.TextAlign.Companion.Justify
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -50,21 +50,21 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun InicializaTablas(){
-    imaxes = arrayOf<Painter>(
+    imaxes = arrayOf(
         painterResource(R.drawable.lemon_tree),
         painterResource(R.drawable.lemon_squeeze),
         painterResource(R.drawable.lemon_drink),
         painterResource(R.drawable.lemon_restart)
     )
 
-    descricions = arrayOf<String>(
+    descricions = arrayOf(
         stringResource(R.string.descricion_1),
         stringResource(R.string.descricion_2),
         stringResource(R.string.descricion_3),
         stringResource(R.string.descricion_4)
     )
 
-    etiquetas = arrayOf<String>(
+    etiquetas = arrayOf(
         stringResource(R.string.etiqueta_1),
         stringResource(R.string.etiqueta_2),
         stringResource(R.string.etiqueta_3),
@@ -86,19 +86,31 @@ fun LemonApp() {
 @Composable
 fun PintaPantalla(modifier: Modifier = Modifier){
     var indice by remember { mutableStateOf(0) }
+    var aleatorio by remember {mutableStateOf((1..3).random())}
+    var veces = 0
+
     Column(modifier = modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Image(modifier = Modifier
-            .clickable ( enabled = true, onClick = {
-                    indice++
-                    if (indice == 4){
-                        indice = 0
+            .clickable(enabled = true, onClick = {
+                    if (indice == 1 && veces != aleatorio) {
+                        veces++
+                    }
+                    else {
+                        if (indice == 1) {
+                            aleatorio = (1..3).random()
+                        }
+                        indice++
+                        if (indice == 4) {
+                            indice = 0
+                        }
                     }
                 }
             )
-            .border(1.dp, Color.Red, shape = CutCornerShape(5.dp)),
+            .border(1.dp, Color.Blue, shape = CutCornerShape(5.dp))
+            .wrapContentSize(),
             painter = imaxes[indice],
             contentDescription = descricions[indice]
         )
