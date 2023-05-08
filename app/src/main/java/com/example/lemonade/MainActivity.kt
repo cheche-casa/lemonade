b3006248c8e7ab69a0419df587f75e65c9de6db1
cheche-casa <cheche.casa@outlook.es>
2023-05-08 17:33:44 +0200

    Cambio de imaxe on click

--- app/src/main/java/com/example/lemonade/MainActivity.kt
+++ app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,12 +12,15 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.lemonade.ui.theme.LemonadeTheme
 
@@ -67,17 +71,18 @@ fun LemonApp() {
         color = MaterialTheme.colorScheme.background
     ) {
         InicializaTablas()
-        PintaPantalla(indice = 0)
+        PintaPantalla()
     }
 }
 
 @Composable
-fun PintaPantalla(indice: Int, modifier: Modifier = Modifier){
+fun PintaPantalla(modifier: Modifier = Modifier){
+    var indice by remember { mutableStateOf(0) }
     Column(modifier = modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Image(
+        Image(modifier = Modifier.clickable ( enabled = true, onClick = {indice++} ),
             painter = imaxes[indice],
             contentDescription = descricions[indice]
         )
